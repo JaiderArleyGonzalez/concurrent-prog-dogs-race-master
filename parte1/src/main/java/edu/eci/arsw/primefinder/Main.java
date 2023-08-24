185fdf716908411f12271c36226168f6f04e2d7f
unknown <jaider.gonzalez@labinfo.is.escuelaing.edu.co>
2023-08-24 09:59:02 -0500

    Adelanto en clase

--- parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/Main.java
@@ -1,14 +1,57 @@
 package edu.eci.arsw.primefinder;
 
+import java.util.*;
+
 public class Main {
+	private static final int numberOfThreads = 3;
+	private static final int finalNumber = 30000000;
+	private static List<Integer> primes = new LinkedList<Integer>();
 
 	public static void main(String[] args) {
-		
-		PrimeFinderThread pft=new PrimeFinderThread(0, 30000000);
-		
-		pft.start();
-		
-		
+		int startIndex, endIndex;
+		int chunkSize = finalNumber / numberOfThreads;
+
+		ArrayList<PrimeFinderThread> threads = new ArrayList<PrimeFinderThread>();
+		for (int i = 0; i < numberOfThreads; i++) {
+			startIndex = i * chunkSize;
+			endIndex = (i == numberOfThreads - 1) ? finalNumber : (i + 1) * chunkSize;
+			PrimeFinderThread thread = new PrimeFinderThread(startIndex, endIndex, "Hilo N°" + (i + 1));
+			threads.add(thread);
+		}
+		for (PrimeFinderThread thread : threads) {
+			thread.start();
+			thread.setStartTime(System.currentTimeMillis());
+		}
+
+		int count = 0;
+		int countAlive = 0;
+		while (countAlive <= numberOfThreads) {
+			for (PrimeFinderThread thread : threads) {
+				if (thread.getTime() >= 5 && count <= numberOfThreads) {
+					count++;
+
+					try {
+						thread.wait();
+						System.out.println("El " + thread.getName() + " encontró:" + thread.getPrimes().toString());
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+				if (!thread.isAlive()) {
+					countAlive++;
+				}
+			}
+			if (count == numberOfThreads) {
+				System.out.println("Presione enter para continuar:");
+				String entradaTeclado = "";
+				Scanner entradaEscaner = new Scanner(System.in); // Creación de un objeto Scanner
+				entradaTeclado = entradaEscaner.nextLine();
+			}
+		}
+
+		// primes.addAll(thread.getPrimes());
+		// System.out.println(primes.toString());
+
 	}
-	
+
 }
--- parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
+++ parte1/src/main/java/edu/eci/arsw/primefinder/PrimeFinderThread.java
@@ -3,44 +3,54 @@ package edu.eci.arsw.primefinder;
 import java.util.LinkedList;
 import java.util.List;
 
-public class PrimeFinderThread extends Thread{
+public class PrimeFinderThread extends Thread {
 
-	
-	int a,b;
-	
-	private List<Integer> primes=new LinkedList<Integer>();
-	
-	public PrimeFinderThread(int a, int b) {
+	private int a, b;
+	private long startTime, endTime;
+	private List<Integer> primes = new LinkedList<Integer>();
+	private String name;
+
+	public PrimeFinderThread(int a, int b, String name) {
 		super();
 		this.a = a;
 		this.b = b;
+		this.name = name;
+
 	}
 
-	public void run(){
-		for (int i=a;i<=b;i++){						
-			if (isPrime(i)){
+	public void run() {
+		for (int i = a; i <= b; i++) {
+			if (isPrime(i)) {
 				primes.add(i);
-				System.out.println(i);
+				// System.out.println(i);
 			}
 		}
-		
-		
+
 	}
-	
-	boolean isPrime(int n) {
-	    if (n%2==0) return false;
-	    for(int i=3;i*i<=n;i+=2) {
-	        if(n%i==0)
-	            return false;
-	    }
-	    return true;
+
+	public synchronized double getTime() {
+		long endTime = System.currentTimeMillis();
+		long elapsedTimeMillis = endTime - startTime;
+		double elapsedTimeSeconds = (double) elapsedTimeMillis / 1000.0;
+		return elapsedTimeSeconds;
+	}
+
+	public void setStartTime(long startTime) {
+		this.startTime = startTime;
+	}
+
+	public boolean isPrime(int n) {
+		if (n % 2 == 0)
+			return false;
+		for (int i = 3; i * i <= n; i += 2) {
+			if (n % i == 0)
+				return false;
+		}
+		return true;
 	}
 
 	public List<Integer> getPrimes() {
 		return primes;
 	}
-	
-	
-	
-	
+
 }
